754fd674a29d22754efd7d7412c1c9059e383b3a
Berkay Vuran <berkaypsy@gmail.com>
2021-04-22 15:21:53 +0300

    commit

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,10 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ variables/src/variables/Main.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Main {
+//reusability
+	public static void main(String[] args) {
+		int sayi = 10;
+		String mesaj = "Öğrenci sayısı : ";
+		
+		System.out.println("Öğrenci Sayısı : " +sayi);
+		System.out.println(mesaj + sayi);
+		System.out.println(sayi);
+		System.out.println(sayi);
+		System.out.println(sayi);
+
+	}
+
+}
