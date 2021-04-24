903ef6783b517b2d0e1aac416348813cfca995ab
Berkay Vuran <berkaypsy@gmail.com>
2021-04-24 15:42:24 +0300

    commit

--- loopDemo/src/loopDemo/Main.java
+++ loopDemo/src/loopDemo/Main.java
@@ -3,10 +3,28 @@ package loopDemo;
 public class Main {
 
 	public static void main(String[] args) {
-		for(int i=1;i<10;i+=2) {
+		// for
+		for (int i = 2; i < 10; i += 2) {
 			System.out.println(i);
 		}
-		System.out.println("Döngü Bitti");
+		System.out.println("For Döngüsü Bitti");
+
+		// while
+		int i = 1;
+		while (i < 10) {
+			System.out.println(i);
+			i += 2;
+		}
+		System.out.println("While Döngüsü Bitti");
+
+		// do-while
+		int j = 100;
+		do {
+			System.out.println("Loglandý");
+			System.out.println(j);
+			j += 2;
+		} while (j < 10);
+		System.out.println("Do-While Döngüsü Bitti");
 	}
 
 }
