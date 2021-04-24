aec443d4abc0d93c0ed8f0444555e729c35ac000
Berkay Vuran <berkaypsy@gmail.com>
2021-04-24 13:19:43 +0300

    commit

---
+++ loopDemo/src/loopDemo/Main.java
@@ -0,0 +1,12 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for(int i=1;i<10;i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("Döngü Bitti");
+	}
+
+}
