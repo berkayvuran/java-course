c9885063eb0f584ef34f73e7e6b0ba23f7121761
Berkay Vuran <berkaypsy@gmail.com>
2021-04-25 13:23:27 +0300

    arkadasSayilar

---
+++ arkadasSayilar/src/arkadasSayilar/Main.java
@@ -0,0 +1,33 @@
+package arkadasSayilar;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//220 ve 284. Kendileri hariç pozitif tam bölenlerinin toplamý birbirine eþit olan sayýlar.
+		
+		int sayi1 = 220;
+		int sayi2 = 284;
+		int toplam1 = 0;
+		int toplam2 = 0;
+		
+		for(int i=1;i<sayi1;i++) {
+			if(sayi1%i==0) {
+				toplam1 = toplam1+i;
+			}
+		}
+
+		for(int i=1;i<sayi1;i++) {
+			if(sayi2%i==0) {
+				toplam2 = toplam2+i;
+			}
+		}
+		
+		if(sayi1==toplam2 && sayi2==toplam1) {
+			System.out.println("Bu iki sayý arkadaþtýr");
+		}else {
+			System.out.println("Bu iki sayý arkadaþ deðildir");
+		}
+		
+	}
+
+}
