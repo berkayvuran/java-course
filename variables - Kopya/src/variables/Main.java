04981c6747789f2b40268e4c9433a58029d6cbbf
Berkay Vuran <berkaypsy@gmail.com>
2021-04-28 16:40:45 +0300

    commit

---
+++ variables - Kopya/src/variables/Main.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double sayi = 12.5;
+		sayi = -129;
+		
+		String þehir = "ANKARA";
+		char karakter = 'A';
+		
+		boolean dogruMu = false;
+	}
+
+}
