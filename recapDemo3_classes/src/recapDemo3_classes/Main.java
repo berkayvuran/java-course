b55ab3539132324105d8bba97577ff527136beaa
Berkay Vuran <berkaypsy@gmail.com>
2021-04-28 18:55:41 +0300

    dortIslem

---
+++ recapDemo3_classes/src/recapDemo3_classes/Main.java
@@ -0,0 +1,11 @@
+package recapDemo3_classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.topla(3, 4);
+		System.out.println(sonuc);
+	}
+
+}
