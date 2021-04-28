d2cdba38912fa87a2360f304a9238f37df46d99a
Berkay Vuran <berkaypsy@gmail.com>
2021-04-28 18:27:48 +0300

    classes2

--- classes/src/classes/Main.java
+++ classes/src/classes/Main.java
@@ -3,7 +3,13 @@ package classes;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		//classlar reference type'tır.
+		CustomerManager customerManager = new CustomerManager();
+		CustomerManager customerManager2 = new CustomerManager();
+		customerManager = customerManager2;
+		customerManager.add();
+		customerManager.remove();
+		customerManager.update();
 	}
 
 }
