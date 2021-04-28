cb0b7139933bfcda992c4488c388fd963db8dd47
Berkay Vuran <berkaypsy@gmail.com>
2021-04-28 18:03:01 +0300

    classes1

---
+++ classes/src/classes/CustomerManager.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class CustomerManager {
+	public void add() {
+		System.out.println("Müþteri eklendi.");
+	}
+	
+	public void remove() {
+		System.out.println("Müþteri silindi.");
+	}
+	public void update() {
+		System.out.println("Müþteri güncellendi.");
+	}
+}
---
+++ classes/src/classes/Main.java
@@ -0,0 +1,9 @@
+package classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
