b98ca64fcf75347e4a3ba820d3cad688a460dcd3
Berkay Vuran <berkaypsy@gmail.com>
2021-04-28 19:05:54 +0300

    classWithAttributes

---
+++ classesWithAttributes/src/classesWithAttributes/Main.java
@@ -0,0 +1,19 @@
+package classesWithAttributes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product product = new Product();
+		product.name = "Laptop";
+		product.id = 1;
+		product.description = "Asus Laptop";
+		product.price = 5000;
+		product.stockAmount = 3;
+		
+		System.out.println(product.name);
+		
+		ProductManager productManager = new ProductManager();
+		productManager.add(product);
+	}
+
+}
---
+++ classesWithAttributes/src/classesWithAttributes/ProductManager.java
@@ -0,0 +1,9 @@
+package classesWithAttributes;
+
+public class ProductManager {
+	public void add(Product product) {
+		//JDBC
+		System.out.println("Þu ürün eklendi: " +product.name);
+	}
+
+}
