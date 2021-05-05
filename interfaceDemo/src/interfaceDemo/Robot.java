a0bba74dac114e0b0a340af5950bb0063d301f85
Berkay Vuran <berkaypsy@gmail.com>
2021-05-05 18:23:50 +0300

    interface

---
+++ interfaceDemo/src/interfaceDemo/Robot.java
@@ -0,0 +1,17 @@
+package interfaceDemo;
+
+public class Robot implements IWorkable, IMaintainable {
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void maintain() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
